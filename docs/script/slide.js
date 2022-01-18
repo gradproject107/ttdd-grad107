38a55ba495ec69f44963fdd92747b396cc3f3012
Constanzia <83115706+Constanzia@users.noreply.github.com>
2022-01-18 16:45:18 +0800

    Create slide.js

---
+++ docs/script/slide.js
@@ -0,0 +1,23 @@
+function init() {
+
+}
+
+
+
+var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+    var i;
+    var x = document.getElementsByClassName("slidepic");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(slideshow, 6000);
+}
+
+
+init();
